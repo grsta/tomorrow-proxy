0bb67466da5ec49f06537946ba76233d636376ff
grsta <Ulrichmanor@gmail.com>
2025-06-15 15:14:23 -0500

    Update tomorrow-proxy-server.js

--- tomorrow-proxy-server.js
+++ tomorrow-proxy-server.js
@@ -5,6 +5,34 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+// Weather code → icon URL map
+const weatherIcons = {
+  1000: "https://yourcdn.com/icons/1000.png",
+  1001: "https://yourcdn.com/icons/1001.png",
+  1100: "https://yourcdn.com/icons/1100.png",
+  1101: "https://yourcdn.com/icons/1101.png",
+  1102: "https://yourcdn.com/icons/1102.png",
+  2000: "https://yourcdn.com/icons/2000.png",
+  2100: "https://yourcdn.com/icons/2100.png",
+  3000: "https://yourcdn.com/icons/3000.png",
+  3001: "https://yourcdn.com/icons/3001.png",
+  3002: "https://yourcdn.com/icons/3002.png",
+  4000: "https://yourcdn.com/icons/4000.png",
+  4001: "https://yourcdn.com/icons/4001.png",
+  4200: "https://yourcdn.com/icons/4200.png",
+  4201: "https://yourcdn.com/icons/4201.png",
+  5000: "https://yourcdn.com/icons/5000.png",
+  5001: "https://yourcdn.com/icons/5001.png",
+  5100: "https://yourcdn.com/icons/5100.png",
+  5101: "https://yourcdn.com/icons/5101.png",
+  6000: "https://yourcdn.com/icons/6000.png",
+  6001: "https://yourcdn.com/icons/6001.png",
+  6200: "https://yourcdn.com/icons/6200.png",
+  6201: "https://yourcdn.com/icons/6201.png",
+  7000: "https://yourcdn.com/icons/7000.png",
+  7101: "https://yourcdn.com/icons/7101.png",
+  7102: "https://yourcdn.com/icons/7102.png"
+};
 
 const API_KEY = 'lXFnAVn8p9WiNDhgjhG9tAvvy0Gf9aFDT';
 
