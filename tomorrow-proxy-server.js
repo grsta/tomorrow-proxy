60c08ab93c9cd5891dbd54a312a9574f68bb0730
grsta <Ulrichmanor@gmail.com>
2025-07-04 17:21:59 -0500

    Update tomorrow-proxy-server.js

--- tomorrow-proxy-server.js
+++ tomorrow-proxy-server.js
@@ -5,36 +5,37 @@ app.use(express.json());
 
 // ✅ WeatherCode → Cloudinary video mapping
 const weatherVideos = {
-  1000: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Sun_uvlifo.mp4",
-  1100: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Partly_Cloudy_xhctwf.mp4",
-  1101: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Mostly_Cloudy_eqabmm.mp4",
-  1001: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Cloudy_uijiry.mp4",
-  1102: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Cumulous_Clouds_Sun_wxvhqe.mp4",
-
-  4000: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Light_Rain_zqjzyv.mp4",
-  4001: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Rain_Wind_dd2hjo.mp4",
-  4200: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Rain_Snow_Shower_o7z9bpj.mp4",
-  4201: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Convective_Rain_asxihh.mp4",
-
-  5000: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Snow_Sun_wdvqphe.mp4",
-  5001: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Flurries_sttkce.mp4",
-  5100: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Light_Snow_gxwdah.mp4",
-  5101: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Snow_Shower_upl6b3.mp4",
-
-  6000: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Freezing_Drizzle_Rain_mrklqd.mp4",
-  6001: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Freezing_Rain_37ffyp.mp4",
-  6200: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Mix_vwmmd.mp4",
-  6201: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Mix_Wind_711qq.mp4",
-
-  7000: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Ice_Pellets_refzrs.mp4",
-  7101: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Heavy_Ice_Pellets_uwhmps.mp4",
-
-  8000: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Thunderstorm_Custom_btjleam.mp4",
-  8001: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Thunderstorm_Wind_Custom_vzqio.mp4",
-  8100: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Dry_Lightning_qgpmrf.mp4",
-
-  2101: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Night_syxpfo.mp4",
-  2103: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Windy_Snow_wadetn.mp4",
+  1000: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Sun_vlifro.mp4",
+  1100: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Partly_Cloudy_xhcdwf.mp4",
+  1101: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Mostly_Cloudy_eqdbmn.mp4",
+  1102: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Cloudy_ujjjyr.mp4",
+  2100: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Mix_rwvamd.mp4",
+  4000: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Light_Rain_azdyyv.mp4",
+  4001: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Rain_I_qijqzs.mp4",
+  4200: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Rain_Shower_j5qs3f.mp4",
+  4201: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Rain_Wind_ddpjhp.mp4",
+  4210: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Thunderstorm_zu58xq.mp4",
+  4211: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Thunderstorm_Wind_vdvny4.mp4",
+  4212: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Thunderstorm_Sun_pgrbjd.mp4",
+  5000: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Snow_apib0s.mp4",
+  5001: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Snow_Shower_uqb03b.mp4",
+  5100: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Light_Snow_gswdxh.mp4",
+  5101: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Thundersnow_tx2ae5.mp4",
+  5110: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Snow_Sun_wdvphe.mp4",
+  5111: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Windy_Snow_wdaet6.mp4",
+  5200: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Rain_Snow_Sun_xoxeqr.mp4",
+  5201: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Rain_Snow_Shower_o77gbf.mp4",
+  6000: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Rain_Snow_Shower_o77gbf.mp4",
+  6001: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Rain_Snow_Shower_o77gbf.mp4",
+  6200: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Rain_Snow_Shower_o77gbf.mp4",
+  6201: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Rain_Snow_Shower_o77gbf.mp4",
+  7000: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Mix_rwvamd.mp4",
+  7101: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Mix_Wind_t711q0.mp4",
+  7110: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Convective_Snow_pubntn.mp4",
+  7111: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Convective_Rain_sasinh.mp4",
+  7112: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Convective_Mix_dhw2p4.mp4",
+  8000: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Dry_Lightning_qcgmuh.mp4",
+  1001: "https://res.cloudinary.com/dfq6hq2eh/video/upload/v1722665111/Weather%20Icons%20MP4/Night_sykp1o.mp4"
 };
 
 app.get("/weather", async (req, res) => {
